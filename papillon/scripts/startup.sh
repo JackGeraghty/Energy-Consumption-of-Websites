26a8df89710f7454d350ddae7664fb0a3066eb05
Jack Geraghty <jack.geraghty@ucdconnect.ie>
2021-02-21 19:15:36 +0000

    Startup script

---
+++ papillon/scripts/startup.sh
@@ -0,0 +1,3 @@
+cd ..
+tsc
+node build/papillon/src/main.js ~/../../usr/bin/google-chrome-stable ~/enterprise-papillon-v5.2/apache-tomcat-9.0.12/bin/startup.sh ~/enterprise-papillon-v5.2/papillon_client/client.jar
